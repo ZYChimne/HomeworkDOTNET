d224aff40798f1e80749c42a4f7f5c3a35e257bf
Cirilla <45280721+ZYChimne@users.noreply.github.com>
2020-09-27 21:31:22 +0800

    Add files via upload

---
+++ _HomeworkDOTNET3/HomeworkDOTNET3/OrderService.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HomeworkDOTNET3
+{
+    public class OrderService
+    {
+        private int id = 0;
+
+        public List<Order> Ol { get; set; } = new List<Order>();
+        private readonly ProductList pl = new ProductList();
+
+        public OrderService(ProductList pl)
+        {
+            this.pl = pl;
+        }
+
+        public void AddOrder(string client, List<ClientDemand> demand)
+        {
+            List<OrderLineDetail> odTemp = new List<OrderLineDetail>();
+            foreach (ClientDemand c in demand)
+            {
+                var price = from product in pl.L where product.Name == c.Name select product.Price;
+                OrderLineDetail ol = new OrderLineDetail(c.Name, price.First(), c.Num);
+                odTemp.Add(ol);
+            }
+            OrderDetail od = new OrderDetail(odTemp);
+            Order o = new Order(id, client, od);
+            Ol.Add(o);
+            id++;
+        }
+
+        public void DeleteOrder(string client)
+        {
+            foreach (Order o in Ol)
+            {
+                if (o.Client == client)
+                    try
+                    {
+                        Ol.Remove(o);
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
+            }
+            Console.WriteLine("Order Not Found");
+        }
+
+        public Order GetOrder(int id = -1, string goodsName = "", string client = "", double money = -1)
+        {
+            //TODO
+            if (id != -1)
+            {
+                var o = from order in Ol where order.Id == id select order;
+                if (o.Any()) return o.First();
+                else Console.WriteLine("Order Not Found");
+            }
+            return null;
+        }
+
+        public void UpdateOrder(string client, List<ClientDemand> l)
+        {
+            foreach (Order o in Ol)
+            {
+                if (o.Client == client)
+                {
+                    foreach (ClientDemand cd in l)
+                    {
+                        var product = from odl in o.Od.L where odl.Name == cd.Name select odl;
+                        if (product.Any())
+                        {
+                            product.First().Num = cd.Num;
+                        }
+                        else
+                        {
+                            var price = from productWant in pl.L where productWant.Name == cd.Name select productWant.Price;
+                            OrderLineDetail odl = new OrderLineDetail(cd.Name, price.First(), cd.Num);
+                            o.Od.L.Add(odl);
+                        }
+                    }
+                }
+                Console.WriteLine("Order Not Found");
+            }
+        }
+
+        public string ToStringLong(string client)
+        {
+            string msg1 = "", msg2 = "";
+            foreach (Order o in Ol)
+            {
+                if (client == o.Client)
+                {
+                    msg1 = o.ToString();
+                    msg2 = o.Od.ToString();
+                }
+            }
+            if (msg1 == "" && msg2 == "") Console.WriteLine("Order Not Found");
+            return msg1 + "\n" + msg2;
+        }
+
+        public string ToStringShort(string client)
+        {
+            string msg1 = "";
+            foreach (Order o in Ol)
+            {
+                if (client == o.Client)
+                {
+                    msg1 = o.ToString();
+                }
+            }
+            if (msg1 == "") Console.WriteLine("Order Not Found");
+            return msg1;
+        }
+
+        public void Export()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream("Order.xml", FileMode.Create))
+                xml.Serialize(fs, Ol);
+        }
+
+        public void Import()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream("Order.xml", FileMode.Open))
+            {
+                List<Order> olTemp = (List<Order>)xml.Deserialize(fs);
+                Ol = olTemp;
+            }
+        }
+    }
+}
---
+++ _HomeworkDOTNET3/HomeworkDOTNET3Tests1/OrderServiceTests.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeworkDOTNET3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDOTNET3.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        
+        [TestMethod()]
+        public void OrderServiceTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            os.DeleteOrder("client2");
+        }
+
+        [TestMethod()]
+        public void GetOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateOrderTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            os.UpdateOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 2),
+                new ClientDemand("phone2",1)
+            });
+        }
+
+        [TestMethod()]
+        public void ToStringLongTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            Console.WriteLine(os.ToStringLong("client2"));
+        }
+
+        [TestMethod()]
+        public void ToStringShortTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            Console.WriteLine(os.ToStringShort("client2"));
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            os.Export();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            ProductList pl = new ProductList();
+            OrderService os = new OrderService(pl);
+            os.AddOrder("client1", new List<ClientDemand>(){
+                new ClientDemand("camera1", 1),
+                new ClientDemand("computer1",1)
+            });
+            os.AddOrder("client2", new List<ClientDemand>(){
+                new ClientDemand("camera2", 1),
+                new ClientDemand("phone2",1)
+            });
+            os.Export();
+            os.Import();
+        }
+    }
+}
